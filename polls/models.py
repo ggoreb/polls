e90d9c10548d24141ba25a0cb4fca14e677ad361
HPE <seorab@naver.com>
2018-10-24 11:45:06 +0900

    first

---
+++ polls/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Ctrl + Alt + L : 소스코드 정렬
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text + ' // ' + str(self.votes)
+
+
+
+
+
